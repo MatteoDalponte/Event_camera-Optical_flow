7976982f79b894eb5ae60dab74d7c8f4e2dcc6b6
mmlab <matteodalponte@gmail.com>
2019-12-10 13:22:53 +0100

    function fix

--- project.py
+++ project.py
@@ -6,14 +6,13 @@ import time
 Ne = 2500   #grandezza degli eventi da considerare ad ogni iterazione per stimare i parametri theta
 
 def warped_event(events, tref, theta_x,theta_y):
+    worped_e = pd.DataFrame(columns=['time', 'x', 'y'])
     for i in range(len(events)):
-        worped_e = pd.DataFrame(columns=['time', 'x', 'y'])
         x = int((events.iloc[i, 1])+(events.iloc[i, 0]-tref)*theta_x)
         y = int((events.iloc[i, 2])+(events.iloc[i, 0]-tref)*theta_y)
         worped_e = worped_e.append({'time': tref, 'x': x, 'y': y}, ignore_index=True)
 
-        #ARRENZIONE il worped event di retutrn deve ritornare interi (pixel!) non double dati dall calcolo!
-        return worped_e
+    return worped_e
 
 
     # grandezza totale immagine 180V x 240H
@@ -37,6 +36,7 @@ def events_to_image(event_list, dt):    # tutti gli eventi all'interno di dt ven
 # data = pd.read_csv('outdoors_walking/events.txt', header = None)
 df_chunk = pd.read_csv(r'outdoors_walking/events.txt', chunksize=4000000, sep="\s+", names=["timestamp", "x", "y", "p"])
 for chunk in df_chunk:
-    events_to_image(chunk.iloc[0:4000000, :], 2500)
-    warped_event(chunk.iloc[0:Ne, :],0,0,0)
+    #events_to_image(chunk.iloc[0:4000000, :], 2500)
+    w_e = warped_event(chunk.iloc[0:Ne, :],0,0,0)
+    print (w_e)
     break
